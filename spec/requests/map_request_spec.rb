676cabfc85c0ea9c1bdb38a4dbbb7ceb10c84349
Brandon Liu <brandonliu@berkeley.edu>
2023-11-23 06:41:40 +0000

    working on tests

--- spec/requests/map_request_spec.rb
+++ spec/requests/map_request_spec.rb
@@ -15,10 +15,29 @@ RSpec.describe 'Map request', type: :request do
     end
   end
 
-  describe 'GET /state/' do
+  describe 'GET /state/CA' do
     it 'returns http success' do
       get '/state/CA'
-      expect(response.status).to eq(302)
+      expect(response).to redirect_to('/')
+      expect(@state).should_not be_nil
+    end
+
+    it 'returns alert when invalid state' do
+      get '/state/ABC'
+      expect(response).to redirect_to('/')
+    end
+  end
+
+  describe 'GET /state/CA/county/001' do
+    it 'returns http success' do
+      get '/state/CA/county/001'
+      expect(response).to redirect_to('/')
+      expect(response.body).should_not be_nil
+    end
+
+    it 'returns alert when invalid county' do
+      get '/state/CA/county/123'
+      expect(response).to redirect_to('/')
     end
   end
 end
