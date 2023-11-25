c3703bd5e6b9103aae82cb95202933e7fd38e16c
Anusha Kansal <anushakansal@berkeley.edu>
2023-11-25 00:47:43 +0000

    adding tests for iter 1 part 3

---
+++ features/step_definitions/county_to_reps_steps.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Given(/^I am on the state map page with state symbol "([^"]*)"$/) do |state_symbol|
+  visit state_map_path(state_symbol)
+end
+
+When(/^I click on a county with state symbol "([^"]*)" and fips code (.*)$/) do |state_symbol, std_fips_code|
+  visit county_path(state_symbol, std_fips_code)
+end
+
+Then(/^show me the page content$/) do
+  puts page.body
+end
+
+# Then(/^I should see "([^"]*)"$/) do |content|
+#   expect(page).to have_content(content)
+# end
