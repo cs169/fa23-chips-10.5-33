ce7f7c4c5ab34357520930a5314b6a41105c5271
Anusha Kansal <anushakansal@berkeley.edu>
2023-11-28 22:57:43 +0000

    [style] fixing style issues

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# frozen_string_literal: true
+
 class NewsItemsController < ApplicationController
   before_action :set_representative
   before_action :set_news_item, only: %i[show]
